2f26964a5f288e1fd04fad205428f97fc4a0e0b2
aljourialenezi <109911436+aljourialenezi@users.noreply.github.com>
2022-08-19 18:48:17 +0300

    cw7

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+function BMI(weight، height) {
+    let result = weight / (height * height); 
+    return result;
+}
 
+console.log(BMI(53,1.55));
+
+function status(bmi){
+if (bmi<18.5) {
+    return"لديك نقص في الوزن"
+} else if (bmi>=18.5 && bmi<25) {
+    return "وزنك صحي"
+} else{
+    return "لديك زيادة في الوزن";
+}
+}
+
+function calculate() {
+    let weight =document.getElementById(weight).value
+    let height  =document.getElementById(height).value
+
+let bmi = BMI(weight,height);
+ 
+let desc = Status(bmi)
+
+let div =document.getElementById('result')
+
+div.innerTEXT = bmi + " ==" + desc
+}
